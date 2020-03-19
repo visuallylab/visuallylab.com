a92c8c8602998cbf803dfe1630db0791c6e6ff3c
TseHang <a54383813@gmail.com>
2020-03-19 11:44:41 +0800

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -20,9 +20,9 @@ module.exports = withCSS(
       return {
         '/': { page: '/' },
         '/demo/index': { page: '/demo' },
-        '/demo/electricity': { page: 'demo/electricity' },
-        '/demo/traffic': { page: 'demo/traffic' },
-        '/contact-us': { page: 'contact-us' },
+        '/demo/electricity': { page: '/demo/electricity' },
+        '/demo/traffic': { page: '/demo/traffic' },
+        '/contact-us': { page: '/contact-us' },
         '/service': { page: '/service' },
       };
     },
